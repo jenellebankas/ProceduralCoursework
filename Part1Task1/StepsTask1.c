1df88a37267df49591c967760146763eaf07f3b6
jenellebankas <139067450+jenellebankas@users.noreply.github.com>
2023-10-28 18:47:50 +0000

    Task1Part1 Done

---
+++ Part1Task1/StepsTask1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+
+    // array of FITNESS_DATA structs
+    FITNESS_DATA fitness[100];
+
+    // variables needed 
+    int buffer_size = 250;
+    char line[buffer_size];
+    int counter = 0;
+    int count;
+
+    // declaring variables needed to temp store 
+    char date[11];
+    char time[6];
+    char steps[100000];
+
+    char filename[] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+
+    // to read each line
+    while (fgets(line, buffer_size, file)) {   
+        // sorting the data in the file
+        tokeniseRecord(line, ",", date, time, steps);
+        
+        // copy the info into the array
+        strcpy(fitness[counter].date, date);
+        strcpy(fitness[counter].time, time);
+        fitness[counter].steps = atoi(steps);
+        counter++;
+    }
+
+    // print the number of records
+    printf("Number of records in file: %d\n", counter);
+
+    // to get the first three rows 
+    for(count = 0; count < 3; count ++) {
+        printf("%s/%s/%d\n",fitness[count].date,fitness[count].time,fitness[count].steps);
+    }
+}
+
