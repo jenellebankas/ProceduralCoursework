3abd7340a92271d627d10a09b135a2950d88c365
jenellebankas <139067450+jenellebankas@users.noreply.github.com>
2023-10-31 22:38:42 +0000

    part 2 started

---
+++ Part1Task1/StepCounter.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+
+int main () {
+
+
+}
+
+switch c { 
+
+    case "A":
+        printf("Input filename: \n");
+        scanf("%s", filename);
+        break;
+    case "B":
+        printf("Total records: \n")
+        break;
+    case "C":
+        printf("Fewest steps: \n")
+        break;
+    case "D":
+        printf("largest steps: \n")
+        break;
+    case "E":
+        printf("mean step count: \n")
+        break;
+    case "F":
+        printf("Longest period start: \n")
+        printf("Longest perod end: \n")
+        break;
+    case "Q":
+        break;
+}
